af707e64ff1e6fc77464d23abef158049abb148d
Pingvu <nhlpool27@gmail.com>
2023-01-20 14:11:40 +0100

    Add project files.

---
+++ uppgifter 5.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace uppgift_5._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Skriv 5 namn: ");
+            string[] namn = new string[5];
+
+            for (int i =0; i < 5; i++ )
+            {
+                namn[i] = Console.ReadLine();
+            }
+            
+                Console.WriteLine(" Dina namn är: ");
+                for (int i = 0; i < 5; i++)
+            
+            {
+                
+                    Console.WriteLine(namn[i]);
+            }
+
+        }
+    }
+}
